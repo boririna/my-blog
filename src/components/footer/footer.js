1e34c8be696c452241f1b589bb6aa25440d55876
Irina Borisova <i.a.borisova@gmail.com>
2024-03-27 16:25:30 +0100

    Создать футер

---
+++ src/components/footer/footer.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = ({ className }) => {
+	const [city, setCity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=Zurich&units=metric&lang=ru&appid=ba9b07620dda78e481d823d822335c3a',
+		)
+			.then((res) => res.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+
+	return (
+		<div className={className}>
+			<div>
+				<p>Блог веб-разработчика</p>
+				<div>i.a.borisova@gmail.com</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleDateString('ru', {
+						day: 'numeric',
+						month: 'long',
+					})}
+				</div>
+				<div>
+					{temperature} градусов, {weather}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	background-color: #fff;
+	box-shadow: 0px 0px 15px #a3a3a3;
+	font-weight: 500;
+`;
