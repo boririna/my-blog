9d6e42a4066dec2989f79f8f4321b008afaf3000
Irina Borisova <i.a.borisova@gmail.com>
2024-06-09 18:44:15 +0200

    Верстка главной страницы

--- src/pages/main/components/post-card/post-card.js
+++ src/pages/main/components/post-card/post-card.js
@@ -1,44 +1,73 @@
 import styled from 'styled-components';
 import { Icon } from '../../../../components';
+import { Link } from 'react-router-dom';
 
-const PostCardContainer = ({
-	className,
-	id,
-	title,
-	imageUrl,
-	publishedAt,
-	commentsCount,
-}) => {
+const PostCardContainer = (props) => {
 	return (
-		<div className={className}>
-			<img src={imageUrl} alt={title} />
-			<div className="post-card-footer">
-				<h3>{title}</h3>
-				<div className="post-card-info">
-					<div className="published-at">
-						<Icon
-							inactive={true}
-							id="fa-calendar-o"
-							size="18px"
-							margin="0 10px 0 0"
-							cursor="initial"
-						/>
-						<p>{publishedAt}</p>
-					</div>
-					<div className="comments-count">
-						<Icon
-							inactive={true}
-							id="fa-calendar-o"
-							size="18px"
-							margin="0 10px 0 0"
-							cursor="initial"
-						/>
-						<p>{commentsCount}</p>
+		<div className={props.className}>
+			<Link to={`/post/${props.id}`}>
+				<img src={props.imageUrl} alt={props.title} />
+				<div className="post-card-footer">
+					<h4>{props.title}</h4>
+					<div className="post-card-info">
+						<div className="published-at">
+							<Icon
+								inactive={true}
+								id="fa-calendar-o"
+								size="18px"
+								margin="0 10px 0 0"
+								cursor="initial"
+							/>
+							<p>{props.publishedAt}</p>
+						</div>
+						<div className="comments-count">
+							<Icon
+								inactive={true}
+								id="fa-comment-o"
+								size="18px"
+								margin="0 10px 0 0"
+								cursor="initial"
+							/>
+							<p>{props.commentsCount}</p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</Link>
 		</div>
 	);
 };
 
-export const PostCard = styled(PostCardContainer)``;
+export const PostCard = styled(PostCardContainer)`
+	width: 280px;
+	display: flex;
+	flex-direction: column;
+	margin: 20px;
+	background-color: #eaecec;
+
+	& .img {
+		display: block;
+	}
+
+	& h4 {
+		margin: 5px 0;
+	}
+
+	& .post-card-footer {
+		margin: 0 10px 10px 10px;
+	}
+
+	& .post-card-info {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	& .published-at {
+		display: flex;
+		align-items: center;
+	}
+
+	& .comments-count {
+		display: flex;
+		align-items: center;
+	}
+`;
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -6,30 +6,44 @@ import styled from 'styled-components';
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
 	const requestServer = useServerRequest();
+	// Если не использовать useEffect, то происходит запрос постов и обновление состояния, обновление состояния запускает ререндер, а ререндер запускает запрос постов. Получается бесконечный цикл.
+	// requestServer('fetchPosts').then((posts) => {
+	// 	setPosts(posts.res);
+	// });
 
+	console.log('posts', posts);
+	// Вопрос. Пустой массив постов выводится два раза и потом полученный массив постов выводится два раза в консоль-логе. Не понятно почему два раза.
 	useEffect(() => {
 		requestServer('fetchPosts').then((posts) => {
 			setPosts(posts.res);
 		});
 	}, [requestServer]);
 
-	console.log('imageUrl', posts);
-
 	return (
 		<div className={className}>
 			<div>Главная страница</div>
-			{posts.map((post) => (
-				<PostCard
-					key={post.id}
-					id={post.id}
-					title={post.title}
-					imageUrl={post.imageUrl}
-					publishedAt={post.publishedAt}
-					commentsCount={post.commentsCount}
-				/>
-			))}
+			<div className="post-list">
+				{posts.map((post) => {
+					return (
+						<PostCard
+							key={post.id}
+							id={post.id}
+							title={post.title}
+							imageUrl={post.imageUrl}
+							publishedAt={post.publishedAt}
+							commentsCount={post.commentsCount}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
 
-export const Main = styled(MainContainer)``;
+export const Main = styled(MainContainer)`
+	& .post-list {
+		display: flex;
+		flex-wrap: wrap;
+		padding: 20px;
+	}
+`;
